dd95a4fadb6625b82e4e41d08e7f9ee1a2f66359
TijnEekhout <t.eekhout08@gmail.com>
2026-08-26 13:40:11 +0200

    updated the cubicBezier import so it is imported from framer-motion

--- app/components/projects/NatlabProject.tsx
+++ app/components/projects/NatlabProject.tsx
@@ -1,5 +1,4 @@
-import { motion } from "framer-motion";
-import { cubicBezier } from "motion";
+import { motion, cubicBezier } from "framer-motion";
 
 export default function NatlabProject() {
   return (
